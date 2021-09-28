ed5179b0c357a8d520006ca6710f003a8f82e0d3
Jack de la Motte <jack.delamotte13@gmail.com>
2021-09-27 20:19:17 -0700

    Adds patrick query and cleans out unused stuff.

--- baby-names.py
+++ baby-names.py
@@ -1,3 +1,7 @@
+# CSC 466 Fall 2021 Lab 1
+# Jack de la Motte (jdelamot@calpoly.edu)
+# Luke Watts (luwatts@calpoly.edu)
+
 import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib import style
@@ -11,8 +15,8 @@ def main():
     tiffany_names = national_names[
                                         (national_names['Name'] == 'Tiffany') 
                                         & (national_names['Gender'] == 'F')
-                                        & (national_names['Year'] > 1950)
-                                        & (national_names['Year'] < 1990)
+                                        & (national_names['Year'] >= 1950)
+                                        & (national_names['Year'] <= 1990)
                                     ]
     
     tiffany_names.head()
@@ -26,67 +30,45 @@ def main():
     plt.ylabel('Tiffanys') 
     plt.title('Number of Female Babies Named Tiffany')  
 
-    plt.show()  
+    plt.show()
 
-    tiffany_state_names = state_names[
-                                            (state_names['Name'] == 'Tiffany') 
-                                            & (state_names['Gender'] == 'F')
-                                            & (state_names['Year'] > 1950)
-                                            & (state_names['Year'] < 1990)
-                                            & (state_names['State'] == 'NY')
-                                        ]
-    
-    tiffany_state_names.head()
-
-    x_axis = tiffany_state_names['Year']
-    y_axis = tiffany_state_names['Count']
-
-    plt.plot(x_axis, y_axis)
-    plt.axvline(x=1961, color='r')
-    plt.xlabel('Year')
-    plt.ylabel('Tiffanys') 
-    plt.title('Number of Female Babies Named Tiffany in NY')  
-
-    plt.show() 
-
-    kobe_names = national_names[
-                                        (national_names['Name'] == 'Kobe') 
-                                        & (national_names['Gender'] == 'M')
-                                        & (national_names['Year'] > 1980)
-                                        & (national_names['Year'] < 2014)
-                                    ]
-    
-    kobe_names.head()
-
-    x_axis = kobe_names['Year']
-    y_axis = kobe_names['Count']
-
-    plt.plot(x_axis, y_axis)
-    plt.axvline(x=1996, color='r')
-    plt.xlabel('Year')
-    plt.ylabel('Kobes') 
-    plt.title('Number of Male Babies Named Kobe')  
-
-    plt.show()  
-
-    kobe_state_names = state_names[
-                                            (state_names['Name'] == 'Kobe') 
+    patrick_state_names = state_names[
+                                            (state_names['Name'] == 'Patrick') 
                                             & (state_names['Gender'] == 'M')
-                                            & (state_names['Year'] > 1980)
-                                            & (state_names['Year'] < 2014)
-                                            & (state_names['State'] == 'CA')
+                                            & (state_names['Year'] >= 1910)
+                                            & (state_names['Year'] <= 1950)
+                                            & 
+                                                ((state_names['State'] == 'NY')
+                                                | (state_names['State'] == 'NH')
+                                                | (state_names['State'] == 'VT')
+                                                | (state_names['State'] == 'ME')
+                                                | (state_names['State'] == 'CT')
+                                                | (state_names['State'] == 'RI')
+                                                | (state_names['State'] == 'MA'))
+                                            
                                         ]
-    
-    kobe_state_names.head()
 
-    x_axis = kobe_state_names['Year']
-    y_axis = kobe_state_names['Count']
+    years = []
+    year_totals = []
+
+    for i in range(1910, 1951):
+        years.append(i)
+        year_totals.append(patrick_state_names.loc[patrick_state_names['Year'] == i, 'Count'].sum())
+
+    df = pd.DataFrame(
+        {
+            'Year': years,
+            'Count': year_totals
+        }
+    )
+
+    x_axis = df['Year']
+    y_axis = df['Count']
 
     plt.plot(x_axis, y_axis)
-    plt.axvline(x=1996, color='r')
     plt.xlabel('Year')
-    plt.ylabel('Kobes') 
-    plt.title('Number of Male Babies Named Kobe in CA')  
+    plt.ylabel('Patricks') 
+    plt.title('Number of Male Babies Named Patrick in New England')  
 
     plt.show() 
 
